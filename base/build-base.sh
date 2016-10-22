6ee24717e48362bcc4fd6dd9033f14d6a6458526
Ivan Porto Carrero <ivan@flanders.co.nz>
2016-10-22 14:31:34 -0700

    update base to be automated build

--- base/build-base.sh
+++ base/build-base.sh
@@ -2,5 +2,5 @@
 
 docker pull alpine
 docker run --rm -it -v "$(pwd):/out" -v "$(pwd)/update-base-bin.sh:/bin/update-base-bin" alpine update-base-bin
-docker build --pull --force-rm --no-cache -t casualjim/base:${1-"latest"} .
-docker push casualjim/base:${1-"latest"}
+# docker build --pull --force-rm --no-cache -t casualjim/base:${1-"latest"} .
+# docker push casualjim/base:${1-"latest"}
--- base/update-base-bin.sh
+++ base/update-base-bin.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
-set -eux -o pipefail 
+set -eux -o pipefail
 
 apk add --no-cache --virtual .base-deps gcc musl-dev make git bash
 
 git clone https://github.com/ncopa/su-exec /usr/src/su-exec
 cd /usr/src/su-exec
-make
-cp ./su-exec /out
+LDFLAGS="-s -w" make su-exec-static
+cp ./su-exec-static /out/su-exec
 
 git clone https://github.com/Yelp/dumb-init /usr/src/dumb-init
 cd /usr/src/dumb-init
 make
 cp ./dumb-init /out
-
